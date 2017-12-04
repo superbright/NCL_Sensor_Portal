768c62eff2deb25e1ac3068f8fcb99538885085d
Kevin Cadena <kevincadena@Kevins-MacBook-Pro-2.local>
2017-12-04 17:27:56 -0500

    refining interval and adding dates

--- semantic/dist/script.js
+++ semantic/dist/script.js
@@ -1,7 +1,17 @@
 // Loading the JSON
 
 $(function() {
+document.getElementById("demo").innerHTML = formatAMPM();
 
+function formatAMPM() {
+var d = new Date(),
+    minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
+    hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
+    ampm = d.getHours() >= 12 ? 'pm' : 'am',
+    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+    days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear()+' '+hours+':'+minutes+ampm;
+}
 
    var menu = [];
 
@@ -63,12 +73,15 @@ function tick() {
                     time = nextTime();
                 chart.push([{ time: time, y: y}]);
                 nextIndex++;
+
             };
 
             pushPoint();
-            interval = setInterval(pushPoint, 100);
+            interval = setInterval(pushPoint, 1000);
 
         };
+
+
 
         console.log(data);
 
