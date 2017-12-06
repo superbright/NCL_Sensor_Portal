63b4a3605b5fed978a87431e2402985b1721a5d1
igal nassima <inassima@gmail.com>
2017-12-05 20:03:21 -0500

    binded the data to the incoming socket data

--- semantic/dist/script.js
+++ semantic/dist/script.js
@@ -21,13 +21,10 @@ return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYe
            $(mnuLst).appendTo("#menu .body");
            $(document).ready(function(){
               $("#" + i).click(function(){
-
+                reset();
                 $('#description').text(f.desc);
               });
             });
-
-
-
      });
 
    });
@@ -35,60 +32,58 @@ return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYe
 
   var t = -1,
   n = 40,
-  duration = 750,
-  data = [];
+  duration = 750;
+  //data = [];
+      var nextTime = (function() {
+      var currentTime = parseInt(new Date().getTime() / 1000);
+      return function() { return currentTime++; }
+  })();
 
-          var nextTime = (function() {
-          var currentTime = parseInt(new Date().getTime() / 1000);
-          return function() { return currentTime++; }
-      })();
+var reset = function() {
+  console.log("ok");
+  socket.removeAllListeners("data1");
+  data = [{ label: 'A', values: [] }],
+    length = 40,
+    nextIndex = length,
+    playing = true,
+    interval = null;
+
+
+   chart = $('#test-3 .epoch').epoch({
+    type: 'time.line',
+    data: data
+  });
+  // And this is required to see the updated styles...
+  chart.redraw();
+}
+
+var data = [{ label: 'A', values: [] }],
+  length = 40,
+  nextIndex = length,
+  playing = true,
+  interval = null;
+
+
+var chart = $('#test-3 .epoch').epoch({
+  type: 'time.line',
+  data: data
+});
+
+var pushPoint = function(val) {
+  var x = nextIndex * 1 * Math.PI / length,
+      y = val,
+      time = nextTime();
+  chart.push([{ time: time, y: y}]);
+  nextIndex++;
+
+};
 
   var socket = io('http://localhost:3000');
   socket.on('data1', function(msg){
-    data.push({
-      time: ++t,
-      value: parseInt(msg.data)/10
-    })
 
-    tick();
-
-
-
-function tick() {
-                var data = [{ label: 'A', values: [] }],
-                length = 40,
-                nextIndex = length,
-                playing = true,
-                interval = null;
-
-
-            var chart = $('#test-3 .epoch').epoch({
-                type: 'time.line',
-                data: data
-            });
-
-            var pushPoint = function() {
-                var x = nextIndex * 2 * Math.PI / length,
-                    y = Math.cos(x) + 1,
-                    time = nextTime();
-                chart.push([{ time: time, y: y}]);
-                nextIndex++;
-
-            };
-
-            pushPoint();
-            interval = setInterval(pushPoint, 100);
-
-        };
-
-
-
-        console.log(data);
-
-
-
-});
-
+    pushPoint(msg.data);
+   console.log(msg.data);
+}); //end socket
 
   // ############## initizalize socket connections
 
